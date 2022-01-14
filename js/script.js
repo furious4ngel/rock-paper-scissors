ca655a749c93e09ace67d3f1452fd8e1d91084c8
t4rantino <stephanie.anoruigwe@gmail.com>
2022-01-14 09:09:37 -0800

    Fix return values for computerPlay function

--- js/script.js
+++ js/script.js
@@ -2,10 +2,11 @@ function computerPlay() {
   let randomNumber = Math.floor(Math.random() * 100) + 1;
   
   if (randomNumber <= (1/3) * 100) {
-    return randomNumber + ' rock';
+    return 'rock';
   } else if (randomNumber <= (2/3) * 100) {
-    return randomNumber + ' paper';
+    return 'paper';
   } else {
-    return randomNumber + ' scissors';
+    return 'scissors';
   }
 }
+
