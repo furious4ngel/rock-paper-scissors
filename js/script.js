12f6f4d391a2d4d3c07e7db6de93978c2731470c
t4rantino <stephanie.anoruigwe@gmail.com>
2022-01-17 10:38:32 -0800

    Remove function declarations and score assignments from game function

--- js/script.js
+++ js/script.js
@@ -1,37 +1,37 @@
+let playerScore = 0;
+let computerScore = 0;
+
+function computerPlay() {
+  let randomNumber = Math.floor(Math.random() * 100) + 1;
+  
+  if (randomNumber <= (1/3) * 100) {
+    return 'Rock';
+  } else if (randomNumber <= (2/3) * 100) {
+    return 'Paper';
+  } else {
+    return 'Scissors';
+  }
+}
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === 'Rock' && computerSelection === 'Scissors' ||
+  playerSelection === 'Paper' && computerSelection === 'Rock' ||
+  playerSelection === 'Scissors' && computerSelection === 'Paper') {
+    playerScore++;
+    return `You win! ${playerSelection} beats ${computerSelection}!`;
+  } else if (computerSelection === 'Rock' && playerSelection === 'Scissors' ||
+  computerSelection === 'Paper' && playerSelection === 'Rock' ||
+  computerSelection === 'Scissors' && playerSelection === 'Paper') {
+    computerScore++;
+    return `You lose! ${computerSelection} beats ${playerSelection}!`;
+  } else {
+    return `Draw! You both chose ${computerSelection}!`;
+  }
+}
+  
 function game() {
   console.clear();
 
-  let playerScore = 0;
-  let computerScore = 0;
-
-  function computerPlay() {
-    let randomNumber = Math.floor(Math.random() * 100) + 1;
-    
-    if (randomNumber <= (1/3) * 100) {
-      return 'Rock';
-    } else if (randomNumber <= (2/3) * 100) {
-      return 'Paper';
-    } else {
-      return 'Scissors';
-    }
-  }
-
-  function playRound(playerSelection, computerSelection) {
-    if (playerSelection === 'Rock' && computerSelection === 'Scissors' ||
-    playerSelection === 'Paper' && computerSelection === 'Rock' ||
-    playerSelection === 'Scissors' && computerSelection === 'Paper') {
-      playerScore++;
-      return `You win! ${playerSelection} beats ${computerSelection}!`;
-    } else if (computerSelection === 'Rock' && playerSelection === 'Scissors' ||
-    computerSelection === 'Paper' && playerSelection === 'Rock' ||
-    computerSelection === 'Scissors' && playerSelection === 'Paper') {
-      computerScore++;
-      return `You lose! ${computerSelection} beats ${playerSelection}!`;
-    } else {
-      return `Draw! You both chose ${computerSelection}!`;
-    }
-  }
-  
   for (let roundNumber = 1; roundNumber <= 5; roundNumber++) {
     let playerSelection = capitalize(prompt('Enter "Rock", "Paper", or "Scissors"', 'Rock'));
     let computerSelection = computerPlay();
